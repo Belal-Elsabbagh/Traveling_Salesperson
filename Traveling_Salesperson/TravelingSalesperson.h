f4e8887a0f28a823fdf158b016aacba42d072f4f
Belal Elsabbagh <70054230+Belal-Elsabbagh@users.noreply.github.com>
2022-06-19 13:57:14 +0200

    Moved documentation inside class

--- Traveling_Salesperson/TravelingSalesperson.h
+++ Traveling_Salesperson/TravelingSalesperson.h
@@ -8,9 +8,32 @@ typedef vector<vector<int>> matrix;
 class TravelingSalesperson
 {
 public:
+	
+	/**
+	 * \brief constructs a tsp solver with an empty graph by taking in the number of nodes.
+	 * 
+	 * \param nNodes The number of nodes in the graph.
+	 */
 	TravelingSalesperson(int size);
+	
+	/**
+	 * \brief constructs a tsp solver using a given graph.
+	 * 
+	 * \param g
+	 */
 	TravelingSalesperson(matrix g);
+	
+	/**
+	 * \brief generates the solution of the shortest path.
+	 * 
+	 * \param start_index the node to start from
+	 */
 	void solve_tsp(int start_index);
+	
+	/**
+	 * \brief prints the generated shortest path solution.
+	 * 
+	 */
 	void print_solution();
 private:
 	size_t numOfNodes;
@@ -19,21 +42,53 @@ private:
 	vector<int> shortest_path, current_path;
 	vector<bool> visited;
 
+	
+	/**
+	 * \brief prints the current path and its cost.
+	 * 
+	 * \param newCost the cost of the current path
+	 */
 	void print_current_solution_data(int newCost);
+	
+	/**
+	 * \brief prints a vector of data.
+	 * \tparam T the element type in the vector
+	 * \param arr the given vector.
+	 */
 	template<class T>
 	void printVector(vector<T> arr);
+	
+	
+	/**
+	 * \brief Checks if node was marked as visited.
+	 * 
+	 * \param node_index
+	 * \return True if node was visited. False otherwise.
+	 */
 	bool isVisited(int node_index);
+	
+	/**
+	 * \brief Checks to see if edge exists between two nodes.
+	 * 
+	 * \param start_node_index
+	 * \param end_node_index
+	 * \return True if an edge exists. False otherwise.
+	 */
 	bool edgeExists(int start_node, int end_node);
+	
+	
 	void save_shortest_path_data(int newCost);
 
+	/**
+	 * \brief calculates the shortest path data and updates it in the shortest path data member of the class..
+	 * 
+	 * \param current_node_index
+	 * \param counter
+	 * \param cost
+	 */
 	void calculate_shortest_path(int currPos = 0, int counter = 0, int cost = 0);
 };
 
-/**
- * \brief constructs a tsp solver with an empty graph by taking in the number of nodes.
- * 
- * \param nNodes The number of nodes in the graph.
- */
 TravelingSalesperson::TravelingSalesperson(int nNodes) : numOfNodes(nNodes),
 shortest_path(nNodes),
 current_path(nNodes),
@@ -42,11 +97,6 @@ graph(nNodes, vector<int>(nNodes, nulledge)),
 ans(MAX)
 {}
 
-/**
- * \brief constructs a tsp solver using a given graph.
- * 
- * \param g
- */
 TravelingSalesperson::TravelingSalesperson(matrix g) : numOfNodes(g.size()),
 shortest_path(numOfNodes),
 current_path(numOfNodes),
@@ -55,11 +105,6 @@ graph(g),
 ans(MAX)
 {}
 
-/**
- * \brief generates the solution of the shortest path.
- * 
- * \param start_index the node to start from
- */
 void TravelingSalesperson::solve_tsp(int start_node)
 {
 	visited[start_node - 1] = true;
@@ -67,43 +112,21 @@ void TravelingSalesperson::solve_tsp(int start_node)
 	shortest_path.push_back(shortest_path[0]);
 }
 
-/**
- * \brief prints the generated shortest path solution.
- * 
- */
 void TravelingSalesperson::print_solution()
 {
 	cout << "Cost of shortest path: " << ans << "\nShortest path: "; printVector(shortest_path);
 }
 
-/**
- * \brief prints the current path and its cost.
- * 
- * \param newCost the cost of the current path
- */
 inline void TravelingSalesperson::print_current_solution_data(int newCost)
 {
 	cout << "path: "; printVector(current_path); cout << "\tcost: " << newCost << "\n";
 }
 
-/**
- * \brief Checks if node was marked as visited.
- * 
- * \param node_index
- * \return True if node was visited. False otherwise.
- */
 inline bool TravelingSalesperson::isVisited(int node_index) 
 {
 	return visited[node_index]; 
 }
 
-/**
- * \brief Checks to see if edge exists between two nodes.
- * 
- * \param start_node_index
- * \param end_node_index
- * \return True if an edge exists. False otherwise.
- */
 inline bool TravelingSalesperson::edgeExists(int start_node_index, int end_node_index) 
 {
 	return graph[start_node_index][end_node_index] != nulledge;
@@ -116,13 +139,6 @@ inline void TravelingSalesperson::save_shortest_path_data(int newCost)
 		shortest_path[i] = current_path[i];
 }
 
-/**
- * \brief calculates the shortest path data and updates it in the shortest path data member of the class..
- * 
- * \param current_node_index
- * \param counter
- * \param cost
- */
 inline void TravelingSalesperson::calculate_shortest_path(int current_node_index, int counter, int cost)
 {
 	if (cost > ans) return;
@@ -147,11 +163,6 @@ inline void TravelingSalesperson::calculate_shortest_path(int current_node_index
 	}
 }
 
-/**
- * \brief prints a vector of data.
- * \tparam T the element type in the vector
- * \param arr the given vector.
- */
 template<class T>
 inline void TravelingSalesperson::printVector(vector<T> arr)
 {
